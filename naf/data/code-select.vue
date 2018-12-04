15111c50a07377585b34cff03da09da114f91d05
dygapp <dygapp@qq.com>
2018-12-05 01:00:00 +0800

    组件内加载字典

--- naf/data/code-select.vue
+++ naf/data/code-select.vue
@@ -1,9 +1,12 @@
 <template>
   <el-select v-model="selected" :placeholder="placeholder || '请选择'" @change="handleChange">
-    <el-option v-for="(item,_index) in $dict(category)" :key="'option-item-'+_index" :label="item.name" :value="item.code" :disabled="item.status == '1'"></el-option>
+    <el-option v-for="(item,_index) in datas" :key="'option-item-'+_index" :label="item.name" :value="item.code"
+               :disabled="item.status == '1'"></el-option>
   </el-select>
 </template>
 <script>
+import { createNamespacedHelpers } from 'vuex';
+const { mapState, mapActions } = createNamespacedHelpers('dict');
 
 export default {
   name: 'code-select',
@@ -17,11 +20,21 @@ export default {
       selected: this.value,
     };
   },
+  async created() {
+    const res = await this.load(this.category);
+    if (!res.errcode) {
+      this.datas.push(res);
+    } else {
+      // eslint-disable-next-line no-console
+      console.error(`数据字典[${this.category}]加载失败：`, res);
+    }
+  },
   methods: {
+    ...mapActions(['load']),
     handleChange() {
       this.$emit('input', this.selected);
       this.$emit('change', this.selected);
-    }
+    },
   },
 };
 </script>
